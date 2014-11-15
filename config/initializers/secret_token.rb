60ed25669c057ab50ee289a480f8ce6033296110
mcousin <martin.cousin@gmx.de>
2014-11-15 15:57:39 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DineAroundTheWorld::Application.config.secret_token = '16ae5ca23b7eb7bd119373d393565f1347e055213a974578ad2e25e669d461b9ede7561f9876ef44f6203779099d158d544f7a05434a4474ca6f7024d25f52fd'
